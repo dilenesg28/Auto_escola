975b3d1075cadaecb611e879e019694c592fbc6d
dilenesg28 <cdilenes28@gmail.com>
2023-05-12 14:23:53 -0300

    incluso opção excluir

--- assets/js/crud.js
+++ assets/js/crud.js
@@ -7,24 +7,26 @@ window.addEventListener("load", () => {
     atualizar()
 })
 
-function atualizar(){
+function atualizar() {
     localStorage.setItem("atividades", JSON.stringify(atividades))
     document.querySelector("#atividades").innerHTML = ""
-    atividades.forEach(atividade => 
+    atividades.forEach(atividade =>
         document.querySelector("#atividades").innerHTML += criarCard(atividade))
+
+
 }
 
 
 function cadastrar() {
     const titulo = document.querySelector("#titulo").value
     const pontos = document.querySelector("#pontos").value
-    const dataI = document.querySelector("#dataI").value    
+    const dataI = document.querySelector("#dataI").value
     const categoria = document.querySelector("#categoria").value
     const obs = document.querySelector("#obs").value
     const modal = bootstrap.Modal.getInstance(document.querySelector("#exampleModal"))
 
     const atividade = { //JSON Java Script Object Notation
-        
+
         titulo,
         pontos,
         dataI,
@@ -54,6 +56,16 @@ function isValid(valor, campo) {
     }
 
 }
+function limparCard(atividade) {
+    document.querySelector("#titulo").value = ""
+    document.querySelector("#categoria").value = ""
+    document.querySelector("dataI").value = ""
+    document.querySelector("#pontos").value = "1"
+    document.querySelector("#obs").value = ""
+}
+
+
+
 
 function criarCard(atividade) {
     let disabled = atividade.concluida ? "disabled" : ""
@@ -61,14 +73,26 @@ function criarCard(atividade) {
     const card = `
         <div class="col-lg-3 col-md-6 col-sm-12">
         <div class="card">
-            <div class="card-header">
+            <div class="card-header"  style="background-color: #4682B4;" style="font-size: medium;">
                 ${atividade.titulo}
             </div>
-            <div class="card-body">
-                <p class="card-text">${atividade.obs}</p>
-                <p class="card-text">${atividade.categoria}</p>
+            <div class="input-group date" data-provide="datepicker">           
+                ${atividade.dataI}
+            </div>
+            <div class="card-body" style="background-color: #B0C4DE;"> 
+                <p class="card-text">${atividade.obs}</p>                 
+            </div>
+            <div class="card-body" style="background-color: #B0C4DE;"> 
+                <span class="badge text-bg-warning">${atividade.categoria}</span>
                 <span class="badge text-bg-warning">${atividade.pontos}pt</span>
-            </div>            
+            </div>       
+            <div class="card-footer" style="background: #6495ED;">
+                <a href="#" class="btn btn-success" title="marcar como concluída">
+                    <i class="bi bi-check2"></i>
+                </a>
+                <a href="#" onClick="apagar(this)" class="btn btn-danger" title="apagar tarefa">
+                    <i class="bi bi-trash3"></i>
+                </a>
             </div> <!-- card footer -->
         </div> <!-- card -->
     </div> <!-- col -->
